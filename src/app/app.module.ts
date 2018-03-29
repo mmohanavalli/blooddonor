f0e0c69b70e93b283d34bdbfa04c408ef7ddf969
Mohanavalli <mmohanavalli@gmail.com>
2018-03-29 16:40:40 +0530

    app component

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,34 +1,91 @@
 import { BrowserModule } from '@angular/platform-browser';
+import { HttpModule } from '@angular/http';
 import { ErrorHandler, NgModule } from '@angular/core';
 import { IonicApp, IonicErrorHandler, IonicModule } from 'ionic-angular';
 
 import { MyApp } from './app.component';
 import { HomePage } from '../pages/home/home';
-import { ListPage } from '../pages/list/list';
 
 import { StatusBar } from '@ionic-native/status-bar';
 import { SplashScreen } from '@ionic-native/splash-screen';
+import { LatestEventPage } from '../pages/latestevent/latestevent';
+import { LatestpostPage } from '../pages/latestpost/latestpost';
+import { SplashPage } from '../pages/splash/splash';
+import { NearesthospitalsPage } from '../pages/nearesthospitals/nearesthospitals';
+import { NearestbloodbanksPage } from '../pages/nearestbloodbanks/nearestbloodbanks';
+import { LauncherPage } from '../pages/launcher/launcher';
+import { LatesteventdetailPage } from '../pages/latesteventdetail/latesteventdetail';
+import { DonorlistPage } from '../pages/donorlist/donorlist';
+import { DonordetailPage } from '../pages/donordetail/donordetail';
+import { DonorregisterPage } from '../pages/donorregister/donorregister';
+import { BloodrequestPage } from '../pages/bloodrequest/bloodrequest';
+import { ServerService } from './server.service';
+import { SeekeeregisterPage } from '../pages/seekerregister/seekerregister';
+import { FormsModule } from '@angular/forms';
+import { FileTransfer, FileUploadOptions, FileTransferObject } from '@ionic-native/file-transfer';
+import { File } from '@ionic-native/file';
+import { Camera } from '@ionic-native/camera';
+import { ConnectivityProvider } from '../providers/connectivity/connectivity';
+import { GoogleMapsProvider } from '../providers/google-maps/google-maps';
+import { LocationsProvider } from '../providers/locations/locations';
+import { Network } from '@ionic-native/network';
+import { Geolocation } from '@ionic-native/geolocation';
 
 @NgModule({
   declarations: [
     MyApp,
     HomePage,
-    ListPage
+    LatestEventPage,
+    LatestpostPage,
+    SplashPage,
+    NearesthospitalsPage,
+    NearestbloodbanksPage,
+    LauncherPage,
+    LatesteventdetailPage,
+    DonorlistPage,
+    DonordetailPage,
+    SeekeeregisterPage,
+    DonorregisterPage,
+    BloodrequestPage
   ],
   imports: [
     BrowserModule,
+    HttpModule,
+    FormsModule,
     IonicModule.forRoot(MyApp),
   ],
   bootstrap: [IonicApp],
   entryComponents: [
     MyApp,
     HomePage,
-    ListPage
+    SplashPage,
+    LatestEventPage,
+    LatestpostPage,
+    NearesthospitalsPage,
+    NearestbloodbanksPage,
+    LauncherPage,
+    LatesteventdetailPage,
+    DonorlistPage,
+    DonordetailPage,
+    DonorregisterPage,
+    SeekeeregisterPage,
+    BloodrequestPage
   ],
   providers: [
     StatusBar,
     SplashScreen,
-    {provide: ErrorHandler, useClass: IonicErrorHandler}
+    ServerService,
+    FileTransfer,
+    FileTransferObject,
+    File,
+    Camera,
+    {provide: ErrorHandler, useClass: IonicErrorHandler},
+    ConnectivityProvider,
+    GoogleMapsProvider,
+    ConnectivityProvider,
+    LocationsProvider,
+    Network,
+    Geolocation
   ]
 })
 export class AppModule {}
--- src/pages/home/home.ts
+++ src/pages/home/home.ts
@@ -1,14 +1,64 @@
-import { Component } from '@angular/core';
-import { NavController } from 'ionic-angular';
+import { Component, OnInit } from '@angular/core';
+import { NavController, IonicPage } from 'ionic-angular';
+import { DonorlistPage } from '../donorlist/donorlist';
+import { DonorregisterPage } from '../donorregister/donorregister';
+import { ServerService } from '../../app/server.service';
 
+@IonicPage()
 @Component({
   selector: 'page-home',
-  templateUrl: 'home.html'
+  templateUrl: 'home.html',
 })
-export class HomePage {
+export class HomePage implements OnInit { 
 
-  constructor(public navCtrl: NavController) {
+  donorData: any[];
+  seekerData: any[];
 
+  IMG_URL :String;
+
+  constructor(public navCtrl: NavController, private serverService: ServerService) {
+    this.IMG_URL = 'https://blooddonorspot.com/images/register/thumb_img/';
   }
 
+  ngOnInit() {
+    this.getRecentDonors();
+    this.getRecentSeekers();
+  }
+
+  gotoDonorListPage() {
+    this.navCtrl.push(DonorlistPage);
+  }
+
+  gotoDonorRegisterPage() {
+    this.navCtrl.push(DonorregisterPage);
+  }
+
+  convertDate(date){
+    let dateArray=date.split("-");
+    let newDate = dateArray[0] + "." + dateArray[1] + "." + dateArray[2];
+    return newDate;
+  }
+
+  getRecentDonors() {
+    this.serverService.getRecentDonorsService()
+      .subscribe(
+        data => {        
+          this.donorData = data;         
+        },
+        error => {
+          console.error("There is some error to get the data");
+        }
+      );
+  }
+  getRecentSeekers() {
+    this.serverService.getRecentSeekersService()
+      .subscribe(
+        data => {        
+          this.seekerData = data;         
+        },
+        error => {
+          console.error("There is some error to get the data");
+        }
+      );
+  }
 }
